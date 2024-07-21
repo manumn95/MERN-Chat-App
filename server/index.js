0a5a63302e9fef2243faf6510bf634e4d2a5cf59
manumn95 <manumnofficial.95@gmail.com>
2024-07-21 14:31:58 +0530

    For AWS

--- server/index.js
+++ server/index.js
@@ -7,20 +7,18 @@ const cookiesParser = require("cookie-parser");
 const { app, server } = require("./socket/index");
 const path = require("path");
 
-// const app = express()
-app.use(express.json());
-const _dirname = path.dirname("");
+const _dirname = __dirname; // Use __dirname
 const buildpath = path.join(_dirname, "../client/build");
-app.use(express.static(buildpath));
-
-
 
 app.use(
   cors({
-    origin: "*",
+    origin: process.env.FRONTEND_URL || "*", // Use environment variable or default to "*"
+    credentials: true,
   })
 );
 
+app.use(express.json());
+app.use(express.static(buildpath));
 app.use(cookiesParser());
 
 const PORT = process.env.PORT || 8080;
@@ -31,11 +29,11 @@ app.get("/", (request, response) => {
   });
 });
 
-//api endpoints
+// API endpoints
 app.use("/api", router);
 
 connectDB().then(() => {
   server.listen(PORT, () => {
-    console.log("server running at " + PORT);
+    console.log("Server running at " + PORT);
   });
 });
